f875f06eb270563ac382462f62b2b29c31693f6e
KodjoB <43495062+KodjoB@users.noreply.github.com>
2021-06-11 22:20:53 +0200

    Add XMLHighScroreData

--- helpers.go
+++ helpers.go
@@ -165,3 +165,64 @@ func (b *OGame) GetPlayerData(playerID int64) (PlayerData, error) {
 func (b *OGame) RegisterTOTP() {
 
 }
+
+type XMLHighscore struct {
+	Position int64 `xml:"position,attr"` //1
+	PlayerID int64 `xml:"id,attr"`       //101137
+	Score    int64 `xml:"score,attr"`    //6841949
+}
+
+// Players represent api result https://s177-en.ogame.gameforge.com/api/highscore.xml?category=1&type=0
+type XMLHighscores struct {
+	Category  int64          `xml:"category,attr"`
+	Type      int64          `xml:"type,attr"`
+	Timestamp int64          `xml:"timestamp,attr"`
+	ServerID  string         `xml:"serverId,attr"`
+	Player    []XMLHighscore `xml:"player"`
+}
+
+//ID	Name
+// 1		Player
+// 2		Alliance
+//correct types are:
+//ID	Name
+// 0	Total
+// 1	Economy
+// 2	Research
+// 3	Military
+// 5	Military Built
+// 6	Military Destroyed
+// 4	Military Lost
+// 7	Honor
+func (b *OGame) getHighscoreData(ID int64, categorieID int64) (XMLHighscores, error) {
+	//https://s177-en.ogame.gameforge.com/api/highscore.xml?category=1&type=0
+	var results XMLHighscores
+	req, err := http.NewRequest("GET", "https://s"+strconv.FormatInt(b.server.Number, 10)+"-"+b.server.Language+".ogame.gameforge.com//api/highscore.xml?category="+strconv.FormatInt(ID, 10)+"&type="+strconv.FormatInt(categorieID, 10), nil)
+	if err != nil {
+		return results, err
+	}
+	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+	req = req.WithContext(b.ctx)
+	resp, err := b.Client.Do(req)
+	if err != nil {
+		return results, err
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			b.error(err)
+		}
+	}()
+	by, err := wrapperReadBody(b, resp)
+	if err != nil {
+		return results, err
+	}
+	b.bytesUploaded += req.ContentLength
+	if err := xml.Unmarshal(by, &results); err != nil {
+		return results, err
+	}
+	return results, nil
+}
+
+func (b *OGame) GethighscoreData(ID int64, categorieID int64) (XMLHighscores, error) {
+	return b.getHighscoreData(ID, categorieID)
+}
